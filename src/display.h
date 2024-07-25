6979b31ae4bd4f900ecd4042f0df5a6ccf40f09f
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2024-07-25 12:36:31 -0500

    Add `Display` struct for double buffering

---
+++ src/display.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDE_GUARD_FC555D26_2ECE_4106_8E9A_4B34829DF67C
+#define INCLUDE_GUARD_FC555D26_2ECE_4106_8E9A_4B34829DF67C
+
+#include <stddef.h>
+
+typedef struct display {
+    size_t width;
+    size_t height;
+    char *cells;
+} Display;
+
+#endif /* !INCLUDE_GUARD_FC555D26_2ECE_4106_8E9A_4B34829DF67C */
