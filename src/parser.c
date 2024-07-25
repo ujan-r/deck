ca3c002194b9f7afcd7df337756c271861ec4cea
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2024-07-25 12:31:33 -0500

    Add Markdown -> `Deck` parsing

---
+++ src/parser.c
@@ -0,0 +1,49 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "deck.h"
+#include "element.h"
+#include "lexer.h"
+#include "parser.h"
+#include "slide.h"
+
+static Slide parseSlide(Element const *, Element const **next);
+
+void parseDeck(char const *text, Deck *deck) {
+    Element const *elements = lex(text, &deck->length);
+
+    deck->slides = calloc(deck->length, sizeof *deck->slides);
+    if (!deck->slides) {
+        fprintf(stderr, "memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t n = 0;
+    while (elements) {
+        deck->slides[n++] = parseSlide(elements, &elements);
+    }
+}
+
+static Slide parseSlide(Element const *elements, Element const **next) {
+    assert(elements);
+
+    size_t count = 0;
+    Element const *e;
+    for (e = elements; e->type != SEP && e->type != END; e++) {
+        count++;
+    }
+
+    *next = e->type == SEP ? e + 1 : NULL;
+
+    Element *array = calloc(count, sizeof *array);
+    if (!array) {
+        fprintf(stderr, "memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    memcpy(array, elements, count * sizeof *array);
+
+    return (Slide){array, count};
+}
