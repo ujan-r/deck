f0e00a6a0051e397500660759da1a7b23aecc992
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2024-07-25 12:28:36 -0500

    Add `Deck` and `Slide` structs

---
+++ src/deck.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_GUARD_A29A70B0_4DF2_4D5E_95E1_39BF7DD22F6E
+#define INCLUDE_GUARD_A29A70B0_4DF2_4D5E_95E1_39BF7DD22F6E
+
+#include <stddef.h>
+
+#include "slide.h"
+
+typedef struct deck {
+    Slide *slides;
+    size_t length;
+} Deck;
+
+#endif /* !INCLUDE_GUARD_A29A70B0_4DF2_4D5E_95E1_39BF7DD22F6E */
---
+++ src/slide.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_GUARD_E39DC1DB_FC34_4454_A196_4904677F8ECB
+#define INCLUDE_GUARD_E39DC1DB_FC34_4454_A196_4904677F8ECB
+
+#include <stddef.h>
+
+#include "element.h"
+
+typedef struct slide {
+    Element *elements;
+    size_t length;
+} Slide;
+
+#endif /* !INCLUDE_GUARD_E39DC1DB_FC34_4454_A196_4904677F8ECB */
