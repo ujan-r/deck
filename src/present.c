6d97a34678200ccdd57391fb058da44d6c16c72c
Ujan RoyBandyopadhyay <116058173+ujan-r@users.noreply.github.com>
2024-07-25 13:00:21 -0500

    Enable `Deck` presentation

--- src/present.c
+++ src/present.c
@@ -1,18 +1,66 @@
 #include <curses.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "deck.h"
+#include "display.h"
+#include "parser.h"
 #include "present.h"
+#include "render.h"
+
+Display display;
+Deck deck;
+size_t currentSlide = 0;
+
+static void gotoSlide(size_t);
+static void refreshScreen(void);
 
 void present(char const *text) {
+    parseDeck(text, &deck);
+
     initscr();
-    noecho();
     cbreak();
+    noecho();
     keypad(stdscr, TRUE);
 
-    printw("%s", text);
-    refresh();
+    getmaxyx(stdscr, display.height, display.width);
+    display.cells = calloc(display.width * display.height, sizeof *display.cells);
+    if (!display.cells) {
+        endwin();
+        fprintf(stderr, "memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    gotoSlide(0);
 
     int c;
-    while ((c = getch()) != 'q') {}
+    while ((c = getch()) != 'q') {
+        switch (c) {
+            case ' ': case KEY_RIGHT:
+                gotoSlide(currentSlide + 1);
+                break;
+            case KEY_LEFT:
+                gotoSlide(currentSlide - 1);
+                break;
+        }
+    }
 
     endwin();
+}
+
+static void gotoSlide(size_t index) {
+    currentSlide = index < deck.length ? index : currentSlide;
+    refreshScreen();
+}
+
+static void refreshScreen(void) {
+    renderSlide(deck.slides[currentSlide], &display);
+
+    for (size_t row = 0; row < display.height; row++) {
+        for (size_t col = 0; col < display.width; col++) {
+            char c = display.cells[row * display.width + col];
+            mvaddch(row, col, c ? c : ' ');
+        }
+    }
+    refresh();
 }
